d36fd25dd198659e25b34eaef1924f032bfdd4c3
Gustavo Fonseca <gus.fonnseca@gmail.com>
2023-07-24 21:58:41 -0300

    chore: setup shadcn ui and pick avatar component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,44 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ['class'],
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
-  darkMode: 'class',
   theme: {
+    colors: {
+      background: '#191919',
+      foreground: '#2C2C2C',
+      white: '#F2F2F2',
+      gray: {
+        100: '#7C7F82',
+      },
+    },
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
     extend: {
-      colors: {
-        background: '#191919',
-        foreground: '#2C2C2C',
-        white: '#F2F2F2',
-        gray: {
-          100: '#7C7F82',
+      keyframes: {
+        'accordion-down': {
+          from: { height: 0 },
+          to: { height: 'var(--radix-accordion-content-height)' },
         },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
       },
     },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate')],
 };
