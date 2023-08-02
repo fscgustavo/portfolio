2a6fa8d1934c4d0cfa82c2857c9bc8b7ead2e9ca
Gustavo Fonseca <gus.fonnseca@gmail.com>
2023-08-01 22:57:08 -0300

    chore: add container style and create contact route

---
+++ app/contact/page.tsx
@@ -0,0 +1,14 @@
+export default function Contact() {
+  return (
+    <div>
+      <h1 className="text-5xl font-semibold text-white">Contact</h1>
+      <p className="mt-6 leading-7 text-white">
+        Do not hesitate to contact me through the form here or by direct email
+        on gus.fonnseca@gmail.com regardless of the subject.
+      </p>
+      <form className="mt-16">
+        <input />
+      </form>
+    </div>
+  );
+}
