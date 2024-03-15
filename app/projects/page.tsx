be88843911bb81be19a09f00ae64e8d78c3ade4d
Gustavo Fonseca <gus.fonnseca@gmail.com>
2024-03-15 19:59:29 -0300

    feat: projects list page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,8 +1,55 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardTitle,
+} from '@/components/ui/card';
+import Image from 'next/image';
+import { projects } from './projects';
+import { Badge } from '@/components/ui/badge';
+import Link from 'next/link';
+
 export default function Projects() {
   return (
     <div className="space-y-14">
       <h1 className="text-5xl font-semibold">Projects</h1>
-      <p className="text-muted-foreground">🚧 Projects coming soon...</p>
+      <ul className="grid gap-3">
+        {projects.map((project) => (
+          <li key={project.preview}>
+            <Link
+              href={project.href}
+              className="block rounded-lg bg-card  hover:bg-accent focus-visible:bg-accent"
+            >
+              <Card className="bg-transparent">
+                <CardContent className="flex gap-5">
+                  <Image
+                    {...project.image}
+                    className="self-start rounded-md object-contain"
+                    alt={project.image.alt}
+                    width="200"
+                    height="150"
+                  />
+                  <div className="grid gap-2">
+                    <CardTitle className="line-clamp-1 text-sm  md:text-base">
+                      {project.title}
+                    </CardTitle>
+                    <CardDescription>{project.description}</CardDescription>
+                    <ul className="flex flex-wrap gap-1">
+                      {project.tags.map((tag) => (
+                        <li className="inline" key={tag}>
+                          <Badge className="rounded bg-transparent">
+                            {tag}
+                          </Badge>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </CardContent>
+              </Card>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ app/projects/projects.ts
@@ -0,0 +1,43 @@
+export const projects = [
+    {
+        href: '/projects/city-wiki',
+        image: {
+            src: '/preview-city-wiki.png',
+            alt: 'Preview of the project City Wiki',
+        },
+        title: 'City Wiki',
+        description: `This coding challenge is about a US city information website. I built responsive web pages, used an API for city data, and integrated AI to create engaging summaries followed by a Google Map's iframe. `,
+        preview: 'https://city-wiki.vercel.app/',
+        sourceCode: 'https://github.com/fscgustavo/city-wiki',
+        tags: [
+            'NextJS',
+            'React',
+            'TypeScript',
+            'TailwindCSS',
+            'ShadcnUI',
+            'Zod',
+            'ESLint',
+        ],
+    },
+    {
+        href: '/projects/fast-food-shop',
+        image: {
+            src: '/preview-fast-food-shop.png',
+            alt: 'Preview of the project Fast Food Shop',
+        },
+        title: 'Fast Food Shop',
+        description: `This coding challenge is about a US city information website. I built responsive web pages, used an API for city data, and integrated AI to create engaging summaries followed by a Google Map's iframe. `,
+        preview: 'https://fast-food-shop.vercel.app/dashboard',
+        sourceCode: 'https://github.com/fscgustavo/fast-food-shop',
+        tags: [
+            'NextJS',
+            'React',
+            'TypeScript',
+            'TailwindCSS',
+            'SWR',
+            'ShadcnUI',
+            'Zod',
+            'ESLint',
+        ],
+    },
+];
